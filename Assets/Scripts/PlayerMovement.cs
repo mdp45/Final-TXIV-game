d344f0defcd58ff1a8c752e26f231858a432f5f0
cptnsqueakyclarkson@hotmail.com <cptnsqueakyclarkson@hotmail.com>
2017-04-15 12:54:17 -0700

    added movement scripts

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody2D playerbody;
+
+
+
+    // Use this for initialization
+
+    void Start()
+    {
+        playerbody = GetComponent<Rigidbody2D>();
+    }
+
+
+
+    // Update is called once per frame
+
+    void FixedUpdate()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(moveX, 0);
+        playerbody.AddForce(movement * speed);
+    }
+}
