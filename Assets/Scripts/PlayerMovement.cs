7968bb084aad3b637026794558625df1f2fe03b4
Marc Daniel Panlilio <mdapanlilio@gmail.com>
2017-04-15 23:16:52 -0700

    Created a projectile test scene, projectile script, edited the movement script, and added assets to use for testing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,9 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody2D playerbody;
 
+    //Used for the projectile
+    public Transform firingPoint;
+    public GameObject bullet;
 
 
     // Use this for initialization
@@ -18,8 +21,6 @@ public class PlayerMovement : MonoBehaviour
         playerbody = GetComponent<Rigidbody2D>();
     }
 
-
-
     // Update is called once per frame
 
     void FixedUpdate()
@@ -27,5 +28,15 @@ public class PlayerMovement : MonoBehaviour
         float moveX = Input.GetAxis("Horizontal");
         Vector2 movement = new Vector2(moveX, 0);
         playerbody.AddForce(movement * speed);
+    }
+
+
+    //Used to create the projectile on button press
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bullet, firingPoint.position, firingPoint.rotation);
+        }
     }
 }
