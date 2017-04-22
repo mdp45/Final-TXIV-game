c74bb6578197859c63e647d7658704fb1ce7e5be
cptnsqueakyclarkson@hotmail.com <cptnsqueakyclarkson@hotmail.com>
2017-04-22 14:20:18 -0700

    Apr 22

---
+++ Assets/Scripts/fireBullet.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireBullet : MonoBehaviour {
+
+	public float bulletForce = 750.0f;
+
+	void OnTriggerEnter2d (Collider2D target) {
+
+		if (target.gameObject.tag == "FirePoint") GetComponent<Rigidbody2D>().AddForce(transform.right * bulletForce);
+	}
+}
